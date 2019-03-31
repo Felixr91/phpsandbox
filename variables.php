c9079c866afd6ca18f2d4a8d61647b0e0cdc2e20
Felix <felixr91@gmail.com>
2019-03-31 15:52:33 -0600

    stringsandvariables

---
+++ variables.php
@@ -0,0 +1,33 @@
+<h1><?php
+
+//single line comment
+#single line comment
+
+/* multi
+line
+
+comment
+*/
+
+$output = "Whatup!"; 
+echo $output;
+
+$num1 = 4;
+$num2 = 10;
+$sum = $num1 + $num2;
+echo $sum;
+
+$string1 = "Hello";
+$string2 = "World";
+$string3 = $string1.' '.$string2;
+$string4 = "$string1 $string2";
+echo $string3;
+echo $string4;
+
+//contants
+define('GREETING', 'Hello there!', true);
+echo GREETING;
+echo greeting;
+
+
+?></h1>
